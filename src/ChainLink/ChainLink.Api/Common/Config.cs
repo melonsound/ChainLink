87755a5068d397b009604910755b44d9e721dd8b
melonsound <ghost.134@hotmail.com>
2021-11-11 16:56:20 +0300

    feat: db config class

---
+++ src/ChainLink/ChainLink.Api/Common/Config.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ChainLink.Api.Common
+{
+    public static class Config
+    {
+        public static string DatabaseConnectionString()
+        {
+            string dbUser = Environment.GetEnvironmentVariable("DB_USER"); 
+            string dbPassword = Environment.GetEnvironmentVariable("DB_PASSWORD");
+            string dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+            string dbPort = Environment.GetEnvironmentVariable("DB_PORT");
+            string dbName = Environment.GetEnvironmentVariable("DB_NAME");
+            
+            string dbConnectionString = $"User ID={dbUser};" +
+                                        $"Password={dbPassword};" +
+                                        $"Host={dbHost};" +
+                                        $"Port={dbPort};" +
+                                        $"Database={dbName};" +
+                                        $"SSL Mode=Require;" +
+                                        $"Trust Server Certificate=true";
+
+            return dbConnectionString;
+        }
+    }
+}
