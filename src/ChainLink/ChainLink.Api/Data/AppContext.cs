79d6628200f05b26d779a7d07081543eff32e7db
melonsound <ghost.134@hotmail.com>
2021-11-16 14:13:35 +0300

    db context

---
+++ src/ChainLink/ChainLink.Api/Data/AppContext.cs
@@ -0,0 +1,15 @@
+﻿using ChainLink.Api.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChainLink.Api.Data
+{
+    public class AppContext : DbContext
+    {
+        public AppContext(DbContextOptions<AppContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Account> Accounts;
+    }
+}
--- src/ChainLink/ChainLink.Api/Startup.cs
+++ src/ChainLink/ChainLink.Api/Startup.cs
@@ -2,21 +2,37 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using ChainLink.Api.Common;
+using ChainLink.Api.Helpers;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using AppContext = ChainLink.Api.Data.AppContext;
 
 namespace ChainLink.Api
 {
     public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
         public void ConfigureServices(IServiceCollection services)
         {
+            var authOptionsCfg = Configuration.GetSection("Auth");
+            
             services.AddControllers();
+
+            services.Configure<AuthOptions>(authOptionsCfg);
+
+            services.AddDbContext<AppContext>(options =>
+                options.UseNpgsql(Config.DatabaseConnectionString()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
